bfa062a3d6926a0352175633e860d8f896072f6a
ARubin0 <alfio.p.rubino@gmail.com>
2021-09-21 15:41:51 -0400

    worked on cookmodule and myDiner

--- myDiner/cooks.py
+++ myDiner/cooks.py
@@ -14,18 +14,33 @@ import appliances
 #   foodOrder is customer order
 #   cookBook  is recipe
 
+#XXX CALL IN MAIN
+
 def gordon (foodOrder,cookBook):
     for appliances, recipes in cookBook.items():
-        print (appliances)
-        for recipe,ingredients in recipes.items():
-            print( recipe,ingredients )
-
+        
+        #print (appliances)
+        # XXX use the cookbook to get the ingredients for the order
+        # XXX cook looks up order in cookbook to find CookItem 
+        # XXX use the cooking implement to make the food and return 
+        # XXX use the cookbook to get the ingredients for the order
+        for cookItem,ingredients in recipes.items():    
+            #print( cookItem,ingredients )
+            #appliances, cookItem, ingredients all combinations 
+            appliances, cookItem, ingredients 
+            if cookItem==foodOrder:
+                print ("gordon thinks to himself, now i can finish cooking this food" )
+                print ("im going to use", ingredients, "in", appliances," to make a" ,cookItem)
+                # XXX use the cookbook to get the ingredients for the order
+            
+            # x=appliances.riceCooker(["beans","meat","smallWater"],cookBook) # XXX use the cooking implement to make the food and return 
+            # y=appliances.microwave(["bugs"         , "bigWater"] ,cookBook)
+            # z=appliances.dessert(["icecream"]                    ,cookBook)
+            
+            foodPlate="brownMush"
+            
+            return #("finished food")?
 """    
-    # XXX cook looks up order in cookbook to find recipe
-    # XXX use the cookbook to get the ingredients for the order, then
-    # XXX use the cooking implement to make the food and return 
-    
-    foodPlate="brownMush"
     
     #XXX call the right appliance with the recipe. for example : 
     #    this goes here...z=appliances.riceCooker(["beans","meat","smallWater"],cookBook)
